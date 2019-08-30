2b37ccc08061e779feafd7a22623607d8bb12aa0
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-08-30 07:53:44 +0200

    support multiple input files, via argv

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
 const fs = require("fs");
 const hilbertCurve = require("hilbert-curve");
 const pako = require("pako");
+const basename = require("path").basename;
 const sharp = require("sharp");
 
-const input = fs.readFileSync("HG00096.mut.gz");
-const genotypes = new TextDecoder("utf-8")
-  .decode(pako.inflate(input))
-  .split("\n");
+// FIXME: configurable
+const IMAGE_RESIZE_WIDTH = 128;
 
-const levels = Math.ceil(Math.log2(Math.sqrt(genotypes.length)));
-const n = 2 ** levels;
-
-const imageData = new Uint8ClampedArray(n * n * 4);
-const genotypeChannel = {
-  "0": "r",
-  "1": "g",
-  "2": "b"
-};
-
-for (index = 0; index < genotypes.length; index += 1) {
-  const point = hilbertCurve.indexToPoint(index, n);
-  imageData[offset(point.x, point.y, "a", n)] = 255;
-  imageData[
-    offset(point.x, point.y, genotypeChannel[genotypes[index]], n)
-  ] = 255;
+if (process.argv.length === 2) {
+  console.log(
+    `Usage: ${basename(process.argv[1])} <file1.gz> [<file2.gz>, ...]`
+  );
 }
 
-const img = sharp(Buffer.from(imageData), {
-  raw: { width: n, height: n, channels: 4 }
-});
+for (const file of process.argv.slice(2)) {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    generateImage(data, file);
+  });
+}
 
-img.toFile("out.png", err => {
-  if (err) {
-    console.error(err);
-  }
-});
+function generateImage(data, file) {
+  // FIXME: gzip should be optional
+  const genotypes = new TextDecoder("utf-8")
+    .decode(pako.inflate(data))
+    .split("\n");
 
-img.resize({ width: 128 }).toFile("out.128x128.png", err => {
-  if (err) {
-    console.error(err);
+  const levels = Math.ceil(Math.log2(Math.sqrt(genotypes.length)));
+  const n = 2 ** levels;
+
+  const imageData = new Uint8ClampedArray(n * n * 4);
+  const genotypeChannel = {
+    "0": "r",
+    "1": "g",
+    "2": "b"
+  };
+
+  for (index = 0; index < genotypes.length; index += 1) {
+    const point = hilbertCurve.indexToPoint(index, n);
+    imageData[offset(point.x, point.y, "a", n)] = 255;
+    imageData[
+      offset(point.x, point.y, genotypeChannel[genotypes[index]], n)
+    ] = 255;
   }
-});
+
+  const img = sharp(Buffer.from(imageData), {
+    raw: { width: n, height: n, channels: 4 }
+  });
+
+  const prefix = file.split(".")[0];
+
+  const x = img.toFile(`${prefix}.png`, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`[done] ${prefix}.png`);
+    }
+  });
+
+  img
+    .resize({ width: IMAGE_RESIZE_WIDTH })
+    .toFile(
+      `${prefix}.${IMAGE_RESIZE_WIDTH}x${IMAGE_RESIZE_WIDTH}.png`,
+      err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(
+            `[done] ${prefix}.${IMAGE_RESIZE_WIDTH}x${IMAGE_RESIZE_WIDTH}.png`
+          );
+        }
+      }
+    );
+}
 
 function offset(column, row, channel, width) {
   const channels = "rgba";
