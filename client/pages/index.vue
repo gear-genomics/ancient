232877f329b83df84b4f0c32f877e0740e2e31ae
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-09-03 07:38:14 +0200

    client: load model

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
   <div>...index page goes here...</div>
 </template>
+
+<script>
+import * as tf from '@tensorflow/tfjs'
+
+export default {
+  async asyncData() {
+    const model = await tf.loadLayersModel('/tfjs_artifacts/model.json')
+    return { model }
+  }
+}
+</script>
+
