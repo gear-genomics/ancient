019a7459cd04987829106ff6cdb3d132f0d3b62f
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-09-17 08:14:57 +0200

    hilbert-curve cli: simplify

--- hilbert-curve/index.js
+++ hilbert-curve/index.js
@@ -8,8 +8,7 @@ const basename = require("path").basename;
 const sharp = require("sharp");
 
 program
-  .option("-g, --grayscale", "grayscale image")
-  .option("-w, --width <number>", "width of resized image", x =>
+  .option("-o, --order <number>", "order of Hilbert curve", x =>
     Number.parseInt(x, 10)
   )
   .option("--no-image", "don't generate images")
@@ -28,78 +27,45 @@ for (const file of files) {
     if (err) {
       throw err;
     }
-    generateImage(data, file);
+    processFile(data, file);
   });
 }
 
-function generateImage(data, file) {
+function processFile(data, file) {
   // FIXME: gzip should be optional
   const parsed = Uint8Array.from(
     new TextDecoder("utf-8").decode(pako.inflate(data)).split("\n")
   );
 
-  let genotypes, binned;
-  if (!program.width) {
-    genotypes = parsed;
-  } else {
-    binned = new Uint8Array(program.width * program.width);
-    binSize = parsed.length / binned.length;
-    for (let i = 0; i < binned.length; i += 1) {
-      const chunk = parsed.slice(
-        Math.ceil(i * binSize),
-        Math.floor((i + 1) * binSize)
-      );
-      if (chunk.length > 0) {
-        // TODO allow additional strategies
-        binned[i] = Math.max(...chunk);
-      }
-    }
-    genotypes = binned;
-  }
+  const genotypes = hilbertCurve.construct(parsed, program.order);
+  const imageData = new Uint8Array(genotypes.length);
+  imageData.fill(255);
 
-  const levels = Math.ceil(Math.log2(Math.sqrt(genotypes.length)));
-  const n = 2 ** levels;
-
-  const imageData = new Uint8ClampedArray(n * n * 4);
-  // r=0, g=1, b=2
-  const genotypeChannel = "rgb";
-
-  for (index = 0; index < genotypes.length; index += 1) {
-    const point = hilbertCurve.indexToPoint(index, n);
-    imageData[offset(point.x, point.y, "a", n)] = 255;
-    if (program.grayscale && genotypes[index] !== 2) {
-      const channelValue = genotypes[index] === 0 ? 255 : 127;
-      imageData[offset(point.x, point.y, "r", n)] = channelValue;
-      imageData[offset(point.x, point.y, "g", n)] = channelValue;
-      imageData[offset(point.x, point.y, "b", n)] = channelValue;
-    } else if (!program.grayscale) {
-      imageData[
-        offset(point.x, point.y, genotypeChannel[genotypes[index]], n)
-      ] = 255;
+  for (let i = 0; i < genotypes.length; i += 1) {
+    if (genotypes[i] === 2) {
+      imageData[i] = 0;
+    } else if (genotypes[i] === 1) {
+      imageData[i] = 127;
     }
   }
 
   const prefix = basename(file).split(".")[0];
-  const row = [prefix];
-  for (let i = 0; i < imageData.length; i += 4) {
-    if (program.grayscale) {
-      if (imageData[i] === 255) {
-        row.push(0);
-      } else if (imageData[i] === 127) {
-        row.push(0.5);
-      } else {
-        row.push(1);
-      }
+  const tsvRow = [prefix];
+  for (let i = 0; i < imageData.length; i += 1) {
+    if (imageData[i] === 0) {
+      tsvRow.push(1);
+    } else if (imageData[i] === 127) {
+      tsvRow.push(0.5);
     } else {
-      row.push(`${imageData[i]},${imageData[i + 1]},${imageData[i] + 2}`);
+      tsvRow.push(0);
     }
   }
 
-  const valueFile = program.width
-    ? `${prefix}.${program.width}x${program.width}.tsv`
+  const valueFile = program.order
+    ? `${prefix}.${2 ** program.order}x${2 ** program.order}.tsv`
     : `${prefix}.tsv`;
 
-  fs.writeFile(valueFile, row.join("\t") + "\n", "utf8", err => {
+  fs.writeFile(valueFile, tsvRow.join("\t") + "\n", "utf8", err => {
     if (err) {
       throw err;
     }
@@ -107,11 +73,15 @@ function generateImage(data, file) {
 
   if (program.image) {
     const img = sharp(Buffer.from(imageData), {
-      raw: { width: n, height: n, channels: 4 }
+      raw: {
+        width: Math.sqrt(imageData.length),
+        height: Math.sqrt(imageData.length),
+        channels: 1
+      }
     });
 
     const imageFile = program.width
-      ? `${prefix}.${program.width}x${program.width}.png`
+      ? `${prefix}.${2 ** program.width}x${2 ** program.width}.png`
       : `${prefix}.png`;
 
     img.toFile(imageFile, err => {
@@ -124,7 +94,6 @@ function generateImage(data, file) {
   }
 }
 
-function offset(column, row, channel, width) {
-  const channels = "rgba";
-  return row * (width * 4) + column * 4 + channels.indexOf(channel);
+function offset(column, row, width) {
+  return row * width + column;
 }
